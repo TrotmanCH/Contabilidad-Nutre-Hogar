2df80866c5de789fef842544152ab8e0836f0d2b
Yoseph Araúz <154104882+Calcifer1331@users.noreply.github.com>
2024-09-21 13:24:16 -0500

    se agrego documentacion

--- SistemaContable/src/main/java/com/nutrehogar/sistemacontable/HibernateUtil.java
+++ SistemaContable/src/main/java/com/nutrehogar/sistemacontable/HibernateUtil.java
@@ -1,28 +1,35 @@
 package com.nutrehogar.sistemacontable;
-
+import lombok.Getter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
 import java.util.function.Consumer;
 
+/**
+ * Utilidad para manejar la configuración y creación de sesiones de Hibernate.
+ * Proporciona métodos para ejecutar transacciones en la base de datos.
+ */
 public class HibernateUtil {
 
+    @Getter
     private static SessionFactory sessionFactory;
 
     static {
         try {
+            // Configura y construye la SessionFactory a partir del archivo de configuración de Hibernate.
             sessionFactory = new Configuration().configure().buildSessionFactory();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
+    /**
+     * Ejecuta una acción dentro de una transacción de Hibernate.
+     *
+     * @param action una función que recibe una sesión de Hibernate y contiene la lógica de negocio que se ejecutará
+     *               dentro de la transacción.
+     */
     public static void ejectaTransaction(Consumer<Session> action) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
@@ -36,5 +43,4 @@ public class HibernateUtil {
             e.printStackTrace();
         }
     }
-
 }
