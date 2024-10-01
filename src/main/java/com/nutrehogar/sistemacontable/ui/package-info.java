cabfcb56aa487e3e541bea403e17a128ea2baa13
Yoseph Araúz <154104882+Calcifer1331@users.noreply.github.com>
2024-10-01 10:05:52 -0500

    Reorganizar la estructura del repositorio

---
+++ src/main/java/com/nutrehogar/sistemacontable/ui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Este paquete contiene las clases relacionadas con la interfaz de usuario (UI)
+ * de la aplicación.
+ *
+ * Incluye formularios y ventanas que permiten a los usuarios interactuar
+ * con el sistema, así como componentes gráficos necesarios para la presentación.
+ */
+package com.nutrehogar.sistemacontable.ui;
