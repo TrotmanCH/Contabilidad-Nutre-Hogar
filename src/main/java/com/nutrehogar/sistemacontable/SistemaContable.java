3b1106f0f2df8ce2c8dc8854683501f7525ed6c7
Yoseph Araúz <154104882+Calcifer1331@users.noreply.github.com>
2024-10-03 20:55:59 -0500

    menora en la configuracion

--- src/main/java/com/nutrehogar/sistemacontable/SistemaContable.java
+++ src/main/java/com/nutrehogar/sistemacontable/SistemaContable.java
@@ -1,20 +1,11 @@
 
 package com.nutrehogar.sistemacontable;
 
-import com.nutrehogar.sistemacontable.persistence.model.CuentaEntity;
 import com.nutrehogar.sistemacontable.persistence.repository.CuentaHibernateRepository;
-
-import java.util.List;
 
 public class SistemaContable {
 
     public static void main(String[] args) {
 
-        CuentaHibernateRepository cuentaRepo = CuentaHibernateRepository.getInstance();
-        cuentaRepo.save(CuentaEntity.builder().nombre("Yose").noCuenta("1093").build());
-        List<CuentaEntity> cuentas = cuentaRepo.findAll();
-        cuentas.forEach(System.out::println);
-
-//        BackupService.backupDatabaseJSON();
     }
 }
