7b087c4cdc70f7ecb44b1abbfd8e689dfd2bed76
Calci <Calcifer1331@hotmail.com>
2025-01-03 09:10:01 -0500

    agrege un dato mas al dto, AsientoId para poder abrir el Asiento en el Formalario y editarlo.
    Formulario ahora puede resibir un id de un Asiento y rellenar sus campos con esos datos.
    
    Desde SistemaContable se puede llamar al metodo editarAsiento.

--- src/main/java/com/nutrehogar/sistemacontable/application/dto/MayorGenDTO.java
+++ src/main/java/com/nutrehogar/sistemacontable/application/dto/MayorGenDTO.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 @EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MayorGenDTO {
+    Integer asientoId;
     LocalDate asientoFecha;
     String asientoNombre;
     String tipoDocumentoNombre;
@@ -23,8 +24,9 @@ public class MayorGenDTO {
     @Setter
     BigDecimal saldo;
 
-    public MayorGenDTO(LocalDate fecha, String asientoNombre, String tipoDocumentoNombre,
+    public MayorGenDTO(Integer asientoId, LocalDate fecha, String asientoNombre, String tipoDocumentoNombre,
                        String codigoCuenta, Integer idTipoCuenta, String referencia, BigDecimal debe, BigDecimal haber) {
+        this.asientoId = asientoId;
         this.asientoFecha = fecha;
         this.asientoNombre = asientoNombre;
         this.tipoDocumentoNombre = tipoDocumentoNombre;
